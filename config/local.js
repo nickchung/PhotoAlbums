ef432801b8c044ed97bfaaaafe19d8405c874483
Nick Chung <nickchung1@gmail.com>
2015-04-12 21:53:46 -0700

    Removed the node_modules

---
+++ config/local.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db : {
+    host		 : 'localhost',
+    //port 	 : 8889,
+    port 		 : 3306,
+    database : 'photoalbums',
+    user     : 'root',
+    password : 'Mvcapp1!'
+  }
+}
--- server.js
+++ server.js
@@ -61,12 +61,11 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
 app.set('port', globals.applicationPort);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
-    var connection  = mysql.createConnection(globals.database);
+    var connection  = mysql.createConnection(globals.database());
     connection.connect(function(err) {
         if(err){
             console.log('error connecting to database:');
